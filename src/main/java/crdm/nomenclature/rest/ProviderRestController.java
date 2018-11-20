94b5d4a85f4c011095780471fb2d9501e71e67fe
erast stinca <estinca@crdm.est>
2018-11-20 17:52:55 +0200

    Restfull crud added to providers and sections

--- src/main/java/crdm/nomenclature/rest/ProviderRestController.java
+++ src/main/java/crdm/nomenclature/rest/ProviderRestController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +17,14 @@ import crdm.nomenclature.entity.Provider;
 import crdm.nomenclature.service.ProviderService;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/providers")
 public class ProviderRestController {
 
 	@Autowired
 	private ProviderService providerService;
 	
 	@CrossOrigin
-	@GetMapping("/providers")
+	@GetMapping("")
 	public List<Provider> all() {
 		
 		List<Provider> providers = providerService.all();
@@ -27,4 +32,48 @@ public class ProviderRestController {
 		return providers;
 	}
 	
+	
+	@GetMapping("/{id}")
+	public Provider get(@PathVariable Integer id) {
+
+		Provider provider = providerService.find(id);
+		if(provider == null) {
+//			throw new ProviderNotFoundException("Provider id not found - " + id);
+			throw new RuntimeException();
+		}
+		
+		return provider;
+	}
+	
+	
+	@PostMapping("")
+	public Provider save(@RequestBody Provider provider) {
+		
+		provider.setId(null);
+		providerService.save(provider);
+		
+		return provider;
+	}
+	
+	@PutMapping("")
+	public Provider update(@RequestBody Provider provider) {
+
+		providerService.save(provider);
+		
+		return provider;
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable int id) {
+
+		Provider provider = providerService.find(id);
+		
+		if(provider == null) {
+//			throw new ProviderNotFoundException("Provider id not found - " + id);
+			throw new RuntimeException();
+		}
+		
+		providerService.delete(id);
+		return "Deleted customer id - " + id;
+	}
 }
---
+++ src/main/java/crdm/nomenclature/rest/SectionRestController.java
@@ -0,0 +1,75 @@
+package crdm.nomenclature.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import crdm.nomenclature.entity.Section;
+import crdm.nomenclature.service.SectionService;
+
+@RestController
+@RequestMapping("/api/sections")
+public class SectionRestController {
+
+	@Autowired
+	private SectionService sectionService;
+	
+	@GetMapping("")
+	public List<Section> all() {
+		
+		return sectionService.all();
+	}
+	
+	@GetMapping("/{id}")
+	public Section get(@PathVariable Integer id) {
+
+		Section section = sectionService.find(id);
+		if(section == null) {
+//			throw new SectionNotFoundException("Section id not found - " + id);
+			throw new RuntimeException();
+		}
+		
+		return section;
+	}
+	
+	
+	@PostMapping("")
+	public Section save(@RequestBody Section section) {
+		
+		section.setId(null);
+		sectionService.save(section);
+		
+		return section;
+	}
+	
+	@PutMapping("")
+	public Section update(@RequestBody Section section) {
+
+		sectionService.save(section);
+		
+		return section;
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable int id) {
+
+		Section section = sectionService.find(id);
+		
+		if(section == null) {
+//			throw new SectionNotFoundException("Section id not found - " + id);
+			throw new RuntimeException();
+		}
+		
+		sectionService.delete(id);
+		return "Deleted customer id - " + id;
+	}
+	
+}
